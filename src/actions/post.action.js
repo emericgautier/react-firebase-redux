ad7ab94e72f8a3df9a101821551f2dad3e545bae
emericgautier <emericgtr@gmail.com>
2022-09-28 09:26:10 +0200

    Create add, edit, delete posts

--- src/actions/post.action.js
+++ src/actions/post.action.js
@@ -1,7 +1,17 @@
-import { collection, doc, getDocs } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
 import { db } from "../utils/firebase.config";
 
 export const GET_POSTS = "GET_POSTS";
+export const ADD_POST = "ADD_POST";
+export const EDIT_POST = "EDIT_POST";
+export const DELETE_POST = "DELETE_POST";
 
 export const getPosts = () => {
   return (dispatch) => {
@@ -11,6 +21,38 @@ export const getPosts = () => {
           type: GET_POSTS,
           payload: res.docs.map((doc) => ({ ...doc.data(), id: doc.id })),
         });
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+export const addPost = (data) => {
+  return (dispatch) => {
+    return addDoc(collection(db, "posts"), data)
+      .then(() => {
+        dispatch({ type: ADD_POST, payload: data });
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+export const editPost = (data) => {
+  console.log(data);
+  return (dispatch) => {
+    return updateDoc(doc(db, "posts", data.id), { message: data.message })
+      .then(() => {
+        dispatch({ type: EDIT_POST, payload: { ...data } });
+      })
+      .catch((err) => console.log(err));
+  };
+};
+
+export const deletePost = (postId) => {
+  console.log(postId);
+  return (dispatch) => {
+    return deleteDoc(doc(db, "posts", postId))
+      .then(() => {
+        dispatch({ type: DELETE_POST, payload: { postId } });
       })
       .catch((err) => console.log(err));
   };
